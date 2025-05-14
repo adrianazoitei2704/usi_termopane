bdf514865cbe3d9ec22189a747feeb02122c39ea
stemy2000 <bstemate2000@yahoo.com>
2025-05-14 21:02:29 +0300

    emailjs

--- src/app/components/galerie/galerie.component.ts
+++ src/app/components/galerie/galerie.component.ts
@@ -10,10 +10,23 @@ import { CommonModule } from '@angular/common';
 })
 export class GalleryComponent {
   images = [
-    { src: 'assets/montaj/montaj1.jpg', alt: 'Imagine 1' },
-    { src: 'assets/montaj/montaj2.jpg', alt: 'Imagine 2' },
-    { src: 'assets/montaj/montaj4.jpg', alt: 'Imagine 3' },
-    { src: 'assets/montaj/montaj5.jpg', alt: 'Imagine 4' },
+    { src: 'assets/montaj/montaj1.jpg', alt: 'Montaj 1' },
+    { src: 'assets/montaj/montaj2.jpg', alt: 'Montaj 2' },
+    { src: 'assets/montaj/montaj4.jpg', alt: 'Montaj 3' },
+    { src: 'assets/montaj/montaj5.jpg', alt: 'Montaj 4' },
+    { src: 'assets/montaj/montaj3.jpg', alt: 'Montaj 5' },
+    { src: 'assets/montaj/montaj6.jpg', alt: 'Montaj 6' },
+    { src: 'assets/montaj/montaj7.jpg', alt: 'Montaj 7' },
+    { src: 'assets/montaj/montaj8.jpg', alt: 'Montaj 8' },
+    { src: 'assets/montaj/montaj9.jpg', alt: 'Montaj 9' },
+    { src: 'assets/montaj/montaj10.jpg', alt: 'Montaj 10' },
+    { src: 'assets/montaj/montaj11.jpg', alt: 'Montaj 11' },
+    { src: 'assets/montaj/montaj12.jpg', alt: 'Montaj 12' },
+    { src: 'assets/montaj/montaj13.jpg', alt: 'Montaj 13' },
+    { src: 'assets/montaj/montaj14.jpg', alt: 'Montaj 14' },
+    { src: 'assets/montaj/montaj15.jpg', alt: 'Montaj 15' },
+    { src: 'assets/montaj/montaj16.jpg', alt: 'Montaj 16' },
+    { src: 'assets/montaj/montaj17.jpg', alt: 'Montaj 17' },
   ];
 
   selectedImage: { src: string; alt: string } | null = null;
@@ -24,8 +37,9 @@ export class GalleryComponent {
     this.isModalOpen = true;  // Open the modal
   }
 
-  closeImage() {
-    this.isModalOpen = false;  // Close the modal
-    this.selectedImage = null; // Optionally reset selected image
-  }
+closeImage(event?: Event) {
+  if (event) event.stopPropagation(); // Prevent modal from closing when clicking inside
+  this.isModalOpen = false;
+}
+
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,7 @@ import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
 import {Validators} from '@angular/forms';
 import { FormControl, FormGroup } from '@angular/forms';  
 import { CommonModule } from '@angular/common'; 
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-header',
@@ -17,13 +18,16 @@ export class HeaderComponent {
   scrolled = false;
 
   private formBuilder = inject(FormBuilder);
-  profileForm = this.formBuilder.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    phone: ['', [Validators.required, Validators.pattern(/^07\d{8}$/)]],
-    agreeTerms: [false, Validators.requiredTrue]
-  });
+   profileForm = this.formBuilder.group(
+    { 
+      firstName: ['', Validators.required], 
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^07\d{8}$/)]],
+      message: ['', Validators.required],
+      agreeTerms: [false, Validators.requiredTrue] 
+    });
+
 
 
   @HostListener('window:scroll', [])
@@ -41,8 +45,25 @@ export class HeaderComponent {
     if (this.profileForm.valid) {
       console.log('Form submitted:', this.profileForm.value);
       // You can also reset form or close modal
-    } else {
-      this.profileForm.markAllAsTouched();
-    }
+    } 
+    emailjs.send('service_a8yp6qu','template_59arr0b',{...this.profileForm.value}, { publicKey: 'Ry10onoHzAIZpM_q5'})
+    .then(
+        () => {
+          console.log('SENT');
+        },
+        (error) => {
+          console.log('FAILED', (error as EmailJSResponseStatus).text);
+        },
+      );
+
+      emailjs.send('service_a8yp6qu','template_atzx9l7',{...this.profileForm.value}, { publicKey: 'Ry10onoHzAIZpM_q5'})
+    .then(
+        () => {
+          console.log('SENT');
+        },
+        (error) => {
+          console.log('FAILED', (error as EmailJSResponseStatus).text);
+        },
+      );
   }
 }
